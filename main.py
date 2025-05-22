67bf7808615660b16f5ad43db6c87d3dc0ddc8c1
Nityanshu Ranawat <64055499+nityanshu31@users.noreply.github.com>
2025-05-22 22:15:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,5 +31,5 @@ async def caption_image(file: UploadFile = File(...)):
 # ✅ Add this block to run without needing uvicorn CLI
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
